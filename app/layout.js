296386ae1e6f7cd1f50fbf87274aad9c3583c393
CodeMatrix1 <srivallabhmanyam27@gmail.com>
2025-06-05 00:08:03 +0530

    transverse compatible

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 // app/layout.js
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ClientLayout from "./Clientlayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClientLayout>{children}</ClientLayout>
+{children}
       </body>
     </html>
   );
